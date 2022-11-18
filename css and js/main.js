f3dfd0706386a7202eb0400be2722a8a16bbaa05
Gabrielle Bertocchi Sales <gabrielle_bertocchi@hotmail.com>
2022-11-18 14:14:43 +0000

    add html, css and js

---
+++ css and js/main.js
@@ -0,0 +1,7 @@
+const avatar = document.querySelector('.conteudo__principal__imagem')
+
+avatar.addEventListener("click", helpJavascript )
+
+function helpJavascript() {
+    window.alert("Hello, you clicked on my avatar and with the help of Javascript I can write this!")
+}
