3cc659ffb3cab7623b7c9b7fa5b042192314c329
Gabrielle Bertocchi Sales <gabrielle_bertocchi@hotmail.com>
2023-02-08 11:18:56 +0000

    .

--- css-js/main.js
+++ css-js/main.js
@@ -5,3 +5,4 @@ avatar.addEventListener("click", helpJavascript )
 function helpJavascript() {
     window.alert("Hello, you clicked on my avatar and with the help of Javascript I can write this!")
 }
+
